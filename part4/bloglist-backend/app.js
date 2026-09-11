b104f955f1d26daf9140323e9d2dfdba632c5169
callmezaa <kenzamariyan32@email.com>
2026-09-11 21:19:11 +0700

    feat(part4): refactor bloglist into modules - config, logger, model, controller, app (4.2)

---
+++ part4/bloglist-backend/app.js
@@ -0,0 +1,26 @@
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+logger.info('connecting to MongoDB...')
+
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
